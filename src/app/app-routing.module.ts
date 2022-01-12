5e7f907f82b9244de6baef94044aa9af21bfd69d
yoshida taiga <yda.tga@gmail.com>
2022-01-12 14:10:31 +0900

    add sign-in button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 import { TestComponent } from './test/test.component';
 
-const routes: Routes = [{ path: '/test', component: TestComponent }];
+const routes: Routes = [
+  { path: 'test', component: TestComponent },
+  { path: '', component: AppComponent },
+  { path: 'sign-in', component: SignInComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
-  styleUrls: ['./test.component.scss']
+  styleUrls: ['./test.component.scss'],
 })
 export class TestComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onClickSignIn() {
+    this.router.navigateByUrl('sign-in');
   }
-
 }
